8388a122a37d05102bb716fd6e5a3a0351171981
cosmos2123 <cosmos2123@naver.com>
2025-04-30 17:27:07 +0900

    feat:제어페이지 작업

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,30 @@
-// src/App.tsx
+// src/App.tsx - RobotControlPage 라우트 추가
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import MainPage from './pages/MainPage'
 import SchedulePage from './pages/schedule/SchedulePage'
-import PlayPage from './pages/PlayPage'  // PlayPage import 추가
 import NavigationBar from './components/common/NavigationBar'
 import TimeSelectPage from './pages/schedule/TimeSelectPage'
+import ControlPage from './pages/ControlPage'
+import PlayPage from './pages/PlayPage'
+import RobotControlPage from './pages/RobotControlPage' // 추가
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col bg-app-dark text-white w-full h-screen overflow-hidden">
-        <div className="flex-1 w-full overflow-y-auto"> {/* 스크롤이 가능하도록 설정 */}
+        <div className="flex-1 w-full overflow-y-auto">
           <Routes>
             <Route path="/" element={<MainPage />} />
             <Route path="/schedule" element={<SchedulePage />} />
             <Route path='/schedule/time-select' element={<TimeSelectPage />} />
-            <Route path="/play" element={<PlayPage />} />  {/* PlayPage 경로 추가 */}
+            <Route path='/control' element={<ControlPage />} />
+            <Route path='/play' element={<PlayPage />} />
+            <Route path='/robot-control' element={<RobotControlPage />} /> {/* 새 라우트 추가 */}
             {/* 추가 경로는 여기에 작성 */}
           </Routes>
         </div>
         
-        {/* 네비게이션 바는 App.tsx에서 관리하여 모든 페이지에서 공통으로 사용 */}
         <NavigationBar />
       </div>
     </Router>
