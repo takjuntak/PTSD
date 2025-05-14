35121e80e4b80a4887fc067b9e5c1aff533e51bb
최연지 <rokmc3038@naver.com>
2025-05-14 10:36:05 +0900

    feat: 수정

--- backend/PTSD/main.py
+++ backend/PTSD/main.py
@@ -29,7 +29,7 @@ app = FastAPI(
 # CORS 미들웨어 추가
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://k12d101.p.ssafy.io"],  # 정확히 지정
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
